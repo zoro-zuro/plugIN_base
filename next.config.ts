a3f6b9dcff2c1967ccdfbd9f93234d5d792a7671
zorozuro <yasinnew72@gmail.com>
2025-12-11 08:10:14 +0530

    bug fix and dependency update 002

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ✅ Add this to ignore TS errors during build
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // ✅ Add this to ignore ESLint errors during build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
